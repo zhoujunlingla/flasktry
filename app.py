7e2409e54c7082255279943452f3a481e09380db
zhoujunlingla <2966279127@qq.com>
2023-04-24 19:13:15 +0800

    前端增加base.html,提供模版功能。

--- app.py
+++ app.py
@@ -65,10 +65,17 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user = user)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user = user , movies = movies)
-
-
+    return render_template('index.html' , movies = movies)
